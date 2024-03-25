bd535a796e110836a4686991be7461fc5da766b2
Aarnow <arnaudfgr.dev@gmail.com>
2024-03-25 18:34:39 +0100

    Initial Project Files

---
+++ Events.cs
@@ -0,0 +1,24 @@
+﻿using Life;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrollBlocker
+{
+    public class Events : ModKit.Helper.Events
+    {
+        public Events(IGameAPI api) : base(api)
+        {
+        }
+
+        public async override void OnMinutePassed()
+        {
+            //vérifier si le joueur est dans la liste noir
+            //si oui, vérifier s'il est toujours présent dans sa cellule
+            //s'il n'est pas dans sa cellule, le re-téléporter + strike
+            //si la personne récidive, bannir perm le joueur
+        }
+    }
+}
---
+++ TrollBlocker.cs
@@ -0,0 +1,24 @@
+﻿using Life;
+using ModKit.Helper;
+using ModKit.Interfaces;
+using ModKit.Internal;
+
+namespace TrollBlocker
+{
+    public class TrollBlocker : ModKit.ModKit
+    {
+        private readonly Events events;
+
+        public TrollBlocker(IGameAPI api) : base(api)
+        {
+            PluginInformations = new PluginInformations(AssemblyHelper.GetName(), "1.0.0", "Aarnow");
+            events = new Events(api);
+        }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+            Logger.LogSuccess($"{PluginInformations.SourceName} v{PluginInformations.Version}", "initialisé");
+        }
+    }
+}
